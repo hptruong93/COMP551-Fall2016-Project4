1f543fe1cbe90bb335e829397df2b36bba77c629
hptruong93 <hptruong93@gmail.com>
2016-12-02 09:55:12 -0500

    Fix typos

--- ar/autoregressive.py
+++ ar/autoregressive.py
@@ -1,18 +1,20 @@
+import numpy as np
 from sklearn import datasets
-from sklearn.model_selection import cross_val_predict
 from sklearn import linear_model
 
-model = linear_model.LinearRegression()
+# model = linear_model.LinearRegression()
 # model = linear_model.Ridge(alpha = 1.0, max_iter = None, tol = 0.001)
-linear_model.Lasso(alpha=0.1)
+model = linear_model.Lasso(alpha=0.1)
 
 
-if __name__ == "___main__":
+if __name__ == "__main__":
     X = [[1,2],[3,4],[5,6]]
     y = [1.5, 3.5, 5.5]
 
     X_test = [[1,2], [7, 8], [9, 10]]
     y_test = [1.5, 7.5, 9.5]
+
+    print "Here"
 
     model.fit(X, y)
     print "Square mean error: %s" % np.mean((model.predict(X_test) - y_test)**2)
